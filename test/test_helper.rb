d7fb98f2cca96787635101b25c9a2ab6487dee12
Rodney Cordoba <rodneycordoba.tecnimaq@gmail.com>
2024-05-10 15:47:20 -0500

    Implementado login avanzado

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -15,6 +15,20 @@ module ActiveSupport
       !session[:user_id].nil?
     end
 
+    # Loguea como un usuario particular
+    def log_in_as(user)
+      session[:user_id] = user.id
+    end
+
     # Add more helper methods to be used by all tests here...
   end
+end
+
+class ActionDispatch::IntegrationTest
+
+  # Loguea como un usuario particular
+  def log_in_as(user, password: 'password', remember_me: '1')
+    post login_path, params: { session: { email: user.email, password: password, remember_me: remember_me}}
+  end
+  
 end
